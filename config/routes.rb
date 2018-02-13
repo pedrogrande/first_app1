0a467ff432875ca50951d460f18337e6fbe643bd
pedrogrande <pete@coderfactory.com>
2018-02-13 21:05:10 +1100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :profiles
+  get 'elephant', to: 'home#index'
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
